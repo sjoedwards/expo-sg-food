34ff00dd46f242ba55223cc84463b4925838c7b5
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-22 15:27:01 +0000

    Updated

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search");
+  };
   return (
     <View style={style.backgroundStyle}>
       <SearchBar
@@ -11,7 +17,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={() => console.log("Submitted")}
       />
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
